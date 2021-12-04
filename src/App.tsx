0847499ecee1cebc5d137fad23a93588c1584aba
VTramon <vitorr.ooliveira@gmail.com>
2021-12-04 14:51:47 -0300

    created the the necessarie components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FormEvent, useState } from 'react';
 
 function App() {
+  const [binary, setBinary] = useState('');
+  const [decimal, setDecimal] = useState<number>();
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    if (binary.match(/^[0-1]+$/) === null) {
+      return <span>numeros binarios contem apenas 0 e 1</span>;
+    }
+
+    const invertedBinary = binary.split('').map(Number).reverse();
+
+    const result = invertedBinary.reduce(
+      (previousValue, currentValue, index) =>
+        previousValue + currentValue * Math.pow(2, index)
+    );
+    setDecimal(result);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bin2Dec</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <h2>conversor de números binários para decimais</h2>
+          <input
+            onChange={(event) => setBinary(event.target.value)}
+            type="string"
+            placeholder="binário"
+          />
+          <input disabled type="number" placeholder="decimal" value={decimal} />
+        </div>
+        <button type="submit">converter</button>
+      </form>
     </div>
   );
 }
